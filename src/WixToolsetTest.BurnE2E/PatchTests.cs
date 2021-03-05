b80be2d0b83ed87f187cd2f53a18d7d4ec2c184a
Sean Hall <r.sean.hall@gmail.com>
2021-03-04 19:18:59 -0600

    Enable CanPatchSwidTag test and building SoftwareTags.

--- src/WixToolsetTest.BurnE2E/PatchTests.cs
+++ src/WixToolsetTest.BurnE2E/PatchTests.cs
@@ -55,7 +55,7 @@ namespace WixToolsetTest.BurnE2E
             packageAv1.VerifyTestRegistryRootDeleted();
         }
 
-        [Fact(Skip = "https://github.com/wixtoolset/issues/issues/5949")]
+        [Fact]
         public void CanPatchSwidTag()
         {
             var originalVersion = "1.0.0.0";
